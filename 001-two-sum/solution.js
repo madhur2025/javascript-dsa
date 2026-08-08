c46a3429cc8cee41942a05a30a9413bd9c52ea81
madhur2025 <madhusudansharma2324@gmail.com>
2026-08-08 09:00:45 +0530

    new if code for third question

--- 001-two-sum/solution.js
+++ 001-two-sum/solution.js
@@ -1,5 +1,5 @@
 // one given array of integers return an array with indices of two numbers of array which sum are equal to the target value.
-let nums = [2, 7, 11, 15]
+
 var twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < i; j++) {
@@ -11,6 +11,6 @@ var twoSum = function (nums, target) {
     }
 };
 
-twoSum(nums, 9)
+console.log(twoSum([2, 7, 11, 15], 9))
 
 // isme brute force approch : kisi problem ko solve krne ka sbase seedh or simple trika, bina optimization ki chinta k. mtlb pahle logic sahi hona chaiye, baad me fast banana hai.
